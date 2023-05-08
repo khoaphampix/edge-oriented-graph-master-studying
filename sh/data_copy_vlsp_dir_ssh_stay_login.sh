c93811b8aacd43afcca3635e6b4f9d846c20e290
joekhoa <khoaph92@gmail.com>
2023-05-08 18:09:14 +0700

    003-5 remove log

---
+++ sh/data_copy_vlsp_dir_ssh_stay_login.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+source .env
+remote_host=$HOST_IP
+
+# remote_host=65.109.75.59
+remote_user="root"
+command_to_run=""
+
+# Login to remote server and run command
+ssh -t "$remote_user"@"$remote_host" "cd /home/edge-oriented-graph-master-studying/data/VLSP/processed 
+                                        && cp processed/* .
+                                         ll -ltlh
+                                        && /bin/bash -l"
+

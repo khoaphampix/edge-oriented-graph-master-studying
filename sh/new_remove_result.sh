00a0cb9f72b3e2dfd80853a7ec0e9b02a1e67195
joekhoa <khoaph92@gmail.com>
2023-05-18 22:01:48 +0700

    new-test-store on server, configuration

---
+++ sh/new_remove_result.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+source .env
+remote_host=$HOST_IP
+
+# remote_host=65.109.75.59
+remote_user="root"
+command_to_run=""
+
+# Login to remote server and run command
+# ssh -t "$remote_user"@"$remote_host" "cd /home/edge-oriented-graph-master-studying/data/VLSP/processed 
+#                                         & cp processed/* .
+#                                         & ll -ltlh
+#                                         & /bin/bash -l"
+
+
+# ssh -t "$remote_user"@"$remote_host" "cd /home/edge-oriented-graph-master-studying/results/vlsp-test/ && pwd &&  ls -ltlh && rm -r /home/edge-oriented-graph-master-studying/results/vlsp-test/* && ls"
+
+#!/bin/bash
+
+echo "view log downloaded to local ..."
+
+read -p "delete train log? [y/N] " choice
+if [[ "$choice" =~ ^[Yy]$ ]]; then
+    ssh -t "$remote_user"@"$remote_host" "cd /home/edge-oriented-graph-master-studying/results/vlsp-test/ && pwd &&  ls -ltlh && rm -r /home/edge-oriented-graph-master-studying/results/vlsp-test-new/* && ls"
+
+  echo "DETELE complete."
+else
+  echo "DETELE cancelled."
+fi

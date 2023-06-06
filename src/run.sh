4c90c7e482612967e9148739629e6cb597e9fa32
joekhoa <khoaph92@gmail.com>
2023-06-06 09:46:26 +0700

    set up for dummy debug knowing flow code block

--- src/run.sh
+++ src/run.sh
@@ -1,4 +1,4 @@
-# python3 eog.py --config ../configs/parameters_cdr.yaml --train --gpu 0 --epoch 130   #   MS 
+python3 eog.py --config ../configs/parameters_vlsp_small.yaml --train --gpu -1 --epoch 1   #   MS 
 # python3 eog.py --config ../configs/parameters_cdr.yaml --train --gpu 1 --epoc 100   #   ES
 # python3 eog.py --config ../configs/parameters_cdr.yaml --train --gpu 2 --epoch 80 # MM, MS
 # python3 eog.py --config ../configs/parameters_cdr.yaml --train --gpu 3 --epoch 80 # MM, MS, SS
